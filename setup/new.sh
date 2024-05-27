cb928de39837dc9ea08ac8dfc91095a0474902d8
Brian Davison <b.davison@napier.ac.uk>
2024-05-27 12:49:41 +0100

    Update new.sh: Make jvm and android-sdk locations local to user

--- setup/new.sh
+++ setup/new.sh
@@ -21,7 +21,7 @@ fi
 # echo y | ./sdkmanager --update --sdk_root=/usr/lib/android-sdk/
 # cd ${START_DIR}/Haulage         # change into project root directory
 cd Haulage
-dotnet build -t:InstallAndroidDependencies -f:net8.0-android -p:AndroidSdkDirectory="/usr/lib/android-sdk" -p:JavaSdkDirectory="/usr/lib/jvm" -p:AcceptAndroidSDKLicenses=True
+dotnet build -t:InstallAndroidDependencies -f:net8.0-android -p:AndroidSdkDirectory="${HOME}/android-sdk" -p:JavaSdkDirectory="${HOME}/jvm" -p:AcceptAndroidSDKLicenses=True
 dotnet workload restore         # restore MAUI workloads
 dotnet restore                  # restore project
 echo Done                       # report end of script
