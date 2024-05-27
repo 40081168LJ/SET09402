0d568ac2bda0c8b3fe2ab955229aa0c26d27916d
Brian Davison <b.davison@napier.ac.uk>
2024-05-27 07:36:36 +0000

    Created codespace setup script

--- Haulage/obj/Debug/net8.0-android/Haulage.AssemblyInfo.cs
+++ Haulage/obj/Debug/net8.0-android/Haulage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Haulage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c2683e71902cb8495fec361b299bdbe55ae9f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06ca6bb2295b73e8824c531d51d5c07cb95881d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Haulage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Haulage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ setup/new.sh
@@ -0,0 +1,15 @@
+# Run this script with sudo after the creation of a new codespace
+
+cd Haulage                      # change into project root directory
+dotnet workload restore         # restore MAUI workloads
+dotnet restore                  # restore project
+apt install default-jdk -y      # install jdk
+apt install android-sdk         # install Android sdk
+cd /usr/lib/android-sdk         # change into Android sdk directory
+                                # then download cmdline_tools
+curl -o cli_tools.zip https://bdavison.napier.ac.uk/set09102/setup/commandlinetools-linux-11076708_latest.zip
+unzip cli_tools.zip             # Unzip cmdline_tools (including sdkmanager)
+cd cmdline-tools/bin            # change into cmdline_tools directory
+                                # then update Android platforms to latest
+echo y | ./sdkmanager --update --sdk_root=/usr/lib/android-sdk/
+echo Done                       # report end of script
