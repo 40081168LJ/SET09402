51bc1cc25096156d11c658f14bb42d27cbc896ef
Brian Davison <b.davison@napier.ac.uk>
2024-05-27 12:18:24 +0100

    Update new.sh: Use dotnet android dependencies target

--- setup/new.sh
+++ setup/new.sh
@@ -8,18 +8,20 @@ then
   exit
 fi
 
-START_DIR=$PWD                  # remember starting directory
-apt update                      # update apt repository information
-apt install default-jdk -y      # install jdk
-apt install android-sdk -y      # install Android sdk
-cd /usr/lib/android-sdk         # change into Android sdk directory
-                                # then download cmdline_tools
-curl -o cli_tools.zip https://bdavison.napier.ac.uk/set09102/setup/commandlinetools-linux-11076708_latest.zip
-unzip -o cli_tools.zip          # Unzip cmdline_tools (including sdkmanager)
-cd cmdline-tools/bin            # change into cmdline_tools directory
-                                # then update Android platforms to latest
-echo y | ./sdkmanager --update --sdk_root=/usr/lib/android-sdk/
-cd ${START_DIR}/Haulage         # change into project root directory
+# START_DIR=$PWD                  # remember starting directory
+# apt update                      # update apt repository information
+# apt install default-jdk -y      # install jdk
+# apt install android-sdk -y      # install Android sdk
+# cd /usr/lib/android-sdk         # change into Android sdk directory
+#                                 # then download cmdline_tools
+# curl -o cli_tools.zip https://bdavison.napier.ac.uk/set09102/setup/commandlinetools-linux-11076708_latest.zip
+# unzip -o cli_tools.zip          # Unzip cmdline_tools (including sdkmanager)
+# cd cmdline-tools/bin            # change into cmdline_tools directory
+#                                 # then update Android platforms to latest
+# echo y | ./sdkmanager --update --sdk_root=/usr/lib/android-sdk/
+# cd ${START_DIR}/Haulage         # change into project root directory
+cd Haulage
+dotnet build -t:InstallAndroidDependencies -f:net8.0-android -p:AndroidSdkDirectory="/usr/lib/android-sdk" -p:JavaSdkDirectory="/usr/lib/jvm" -p:AcceptAndroidSDKLicenses=True
 dotnet workload restore         # restore MAUI workloads
 dotnet restore                  # restore project
 echo Done                       # report end of script
