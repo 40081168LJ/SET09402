5d13fb08bad987ea65ffbc85bc5698897a053e49
Brian Davison <b.davison@napier.ac.uk>
2024-05-27 10:51:06 +0100

    Update new.sh" error message + cd to project directory

--- setup/new.sh
+++ setup/new.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/bash -x
-
 # Run this script with sudo after the creation of a new codespace with
-# sudo bash new.sh
+# sudo bash -x new.sh
 
+if [ $(id -u) -ne 0 ]
+then
+  echo "ERROR: This script must be run with sudo"
+  echo "       sudo bash -x $(basename "$0")
+  exit
+fi
+
+START_DIR=$PWD
 apt update                      # update apt repository information
 apt install default-jdk -y      # install jdk
 apt install android-sdk -y      # install Android sdk
@@ -13,7 +19,7 @@ unzip -o cli_tools.zip          # Unzip cmdline_tools (including sdkmanager)
 cd cmdline-tools/bin            # change into cmdline_tools directory
                                 # then update Android platforms to latest
 echo y | ./sdkmanager --update --sdk_root=/usr/lib/android-sdk/
-cd Haulage                      # change into project root directory
+cd ${START_DIR}/Haulage         # change into project root directory
 dotnet workload restore         # restore MAUI workloads
 dotnet restore                  # restore project
 echo Done                       # report end of script
